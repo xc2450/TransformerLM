04f9c03d9c8fac4095df263676708fb044aca6d6
xc2450 <48077531+xc2450@users.noreply.github.com>
2025-12-06 21:29:20 -0500

    Update eval.llm.py

--- eval.llm.py
+++ eval.llm.py
@@ -66,7 +66,7 @@ def main():
         conversation.append({"role": "user", "content": prompt})
 
         templates = {"conversation": conversation, "tokenize": False, "add_generation_prompt": True}
-        if args.weight == 'reason': templates["enable_thinking"] = True # 仅Reason模型使用
+        if args.weight == 'reason': templates["enable_thinking"] = True 
         inputs = tokenizer.apply_chat_template(**templates) if args.weight != 'pretrain' else (tokenizer.bos_token + prompt)
         inputs = tokenizer(inputs, return_tensors="pt", truncation=True).to(args.device)
 
